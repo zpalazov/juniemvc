2bb84cc9812a0f1eecb566213e10fb2dbb96a84d
Zahari Palazov <hophmansoftware@protonmail.com>
2025-09-08 12:59:53 +0300

    Initial commit

---
+++ src/main/kotlin/com/hopman/leaning/juniemvc/JuniemvcApplication.kt
@@ -0,0 +1,11 @@
+package com.hopman.leaning.juniemvc
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JuniemvcApplication
+
+fun main(args: Array<String>) {
+    runApplication<JuniemvcApplication>(*args)
+}
---
+++ src/test/kotlin/com/hopman/leaning/juniemvc/JuniemvcApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.hopman.leaning.juniemvc
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class JuniemvcApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
