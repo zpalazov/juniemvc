78a39d69e347e3a73a3f7bf7beb230312c115f92
Zahari Palazov <hophmansoftware@protonmail.com>
2025-10-19 18:27:11 +0300

    feat: Added JPA audit configuration.

---
+++ src/main/kotlin/com/hophman/learning/juniemvc/config/JpaConfig.kt
@@ -0,0 +1,8 @@
+package com.hophman.learning.juniemvc.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+
+@Configuration
+@EnableJpaAuditing
+internal class JpaConfig
